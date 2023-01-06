bfc51bc5359cbb2a787f36e9fe0b0d75b9b6d204
vale-c <valent95@gmail.com>
2023-01-06 13:58:38 +0100

    added fn to darken panel title color

--- src/Components/Scores.js
+++ src/Components/Scores.js
@@ -12,20 +12,25 @@ let listGroupItemStyle = {
   border: "none",
 };
 
+function darkenColor(color) {
+  const colorValues = color.match(/[a-f\d]{2}/gi).map(hex => parseInt(hex, 16));
+  const darkenedColorValues = colorValues.map(value => Math.floor(value * 0.8));
+  return `#${darkenedColorValues.map(value => value.toString(16)).join('')}`;
+}
+
 class Scores extends Component {
   render() {
     let scoreItems;
     if (this.props.scores) {
       scoreItems = this.props.scores.map((score) => {
         let color = score.color;
-
         let categories = score.name;
 
         let out_of_10 = score.score_out_of_10;
 
         return (
           <Panel accordion="true" key={color} eventKey={color}>
-            <Panel.Heading style={{ color: "#364652" }}>
+            <Panel.Heading style={{ color: darkenColor(color) }}>
               <Panel.Title toggle>{categories}</Panel.Title>
             </Panel.Heading>
             <Panel.Body collapsible>
